9e89dd7a873a5590804b9d2c8cb0c61b1893656b
alinorlopes <alinorlop@outlook.com>
2025-09-06 00:23:43 -0300

    Adicionando sistema de controle

--- src/main/Main.java
+++ src/main/Main.java
@@ -14,9 +14,21 @@ public class Main {
         InterfaceUsuario intuser1 = new InterfaceUsuario();
 
         System.out.println("| Sistema imobiliário |\n" +
-                "Digite quantos financiamentos serão: ");
+                "Digite quantos financiamentos serão: (Máximo de 5 financimentos) ");
 
-        int numeroDeFinancas = scanner.nextInt();
+        int numeroDeFinancas = scanner.nextInt(); // Número de quantos financiamentos serão feitos
+
+        boolean deveFechar = false; //varíavel de controle
+
+        do {
+            if (numeroDeFinancas <= 0 || numeroDeFinancas >= 6) {
+                System.out.println("A quantidade de financiamentos digitados é inválida, Tente novamente.");
+                numeroDeFinancas = scanner.nextInt();
+                deveFechar = false;
+            } else if (numeroDeFinancas >= 1 && numeroDeFinancas <= 5){
+                deveFechar = true;
+            }
+    } while (deveFechar == false); // máximo de 5 financiamentos, não permite negativos e maior que 5
 
         // Acumuladores
         double somaValorImoveis = 0;
@@ -27,13 +39,13 @@ public class Main {
             intuser1.pedirPrazoFinanciamento();
             intuser1.pedirTaxaJurosAnual();
 
-            Financiamento financiamento = new Financiamento(
+            Financiamento financiamento = new Financiamento( //Instância e a criação da finança
                     intuser1.getValorImovel(),
                     intuser1.getPrazoFinanciamento(),
                     intuser1.getTaxaJurosAnual()
             );
 
-            listaDeFinanciamento.add(financiamento);
+            listaDeFinanciamento.add(financiamento); //Adiciona o financiamento na lista
 
             // Atualiza os acumuladores
             somaValorImoveis += intuser1.getValorImovel();
@@ -46,7 +58,6 @@ public class Main {
             System.out.println(f);
 
         }
-
         System.out.println("\nSoma dos valores dos imóveis: R$" + String.format("%.2f", somaValorImoveis));
         System.out.println("Soma dos valores totais dos financiamentos: R$" + String.format("%.2f", somaValorTotalFinanciamentos));
     }
